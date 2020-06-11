3232cf846d2ce1cae618dc84a0949f9cd7a2e695
braydenmackay <braydenmackay1@gmail.com>
2020-06-11 10:49:26 -0600

    added dependencies

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy 
+from flask_cors import CORS
 from flask_marshmallow import Marshmallow 
+from flask_heroku import Heroku
 import os
 
 app = Flask(__name__)
